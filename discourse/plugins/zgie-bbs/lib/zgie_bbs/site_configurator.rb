7a4e2578ff85aa858e9c09fb3b26b0f84c44ae63
Timlin15 <Timlinjq@gmail.com>
2026-09-12 11:58:56 +0800

    chore: remove new tab from discovery navigation

--- discourse/plugins/zgie-bbs/lib/zgie_bbs/site_configurator.rb
+++ discourse/plugins/zgie-bbs/lib/zgie_bbs/site_configurator.rb
@@ -80,6 +80,8 @@ module ZgieBbs
     # already-configured site is a no-op.
     def configure_branding
       SiteSetting.interface_color_selector = "header"
+      SiteSetting.top_menu =
+        (SiteSetting.top_menu.split("|") - ["new"]).join("|")
       images_dir = File.expand_path("../../assets/images", __dir__)
       set_upload_setting(:favicon, File.join(images_dir, "favicon.svg"))
       set_upload_setting(:logo, File.join(images_dir, "logo-light.svg"))
